1ceed1c4db9fb3420b923d27e71adb240d43ec0e
luchofl <lucianofleitas6@hotmail.com>
2022-09-13 16:51:35 -0300

    Proyecto Navbar coderHouse

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="">Flucciano</a>
+    <a className="navbar-brand" href="">FLUCCIANO</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
